2bd9f6ce944c0cc77cd933c97646b090de76b452
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-04-11 10:48:49 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    verdad = x < y
+    falso = y < x
+    print(f'The result of {x} comes first in the dicitionary than {y} is {verdad}.')
+    print(f'The result of {y} comes first in the dicitionary than {x} is {falso}.')
